29a7e23fa1ed43e13530a852b49f476b771c8f17
nelson torres <nelsondaniel88@gmail.com>
2017-10-03 16:41:43 -0400

    delete some code here

--- src/components/seat.js
+++ src/components/seat.js
@@ -6,73 +6,18 @@ class Seat extends Component {
   render() {
     const { id, title, selected } = this.props;
     let className = `list-group-item seat-list col-sm-6`
-
     if (selected) {
       className = `${className} selected`
-    } else if (!selected) {
-      className = `${className} unselected`
     }
-
-    function confirm() {
-      let foo = document.getElementById(id);
-      let btn = document.getElementById(title);
-
-      if ((title === "seat #1") && (selected)) {
-
-        if (window.confirm("Are you sure you want this seat?") === true) {
-          window.alert('you select the seat 1')
-          foo.className = `${className} disabledbutton`;
-          btn.disabled = true;
-        } else {
-          window.alert('you cancel the choose')
-
-        }
-
-      } else if ((title === "seat #2") && (selected)) {
-
-        if (window.confirm("Are you sure you want this seat?") === true) {
-          window.alert('you select the seat 2')
-          foo.className = `${className} disabledbutton`;
-          btn.disabled = true;
-        } else {
-          window.alert('you cancel the choose')
-
-        }
-
-      } else if ((title === "seat #3") && (selected)) {
-
-        if (window.confirm("Are you sure you want this seat?") === true) {
-          window.alert('you select the seat 3')
-          foo.className = `${className} disabledbutton`;
-          btn.disabled = true;
-        } else {
-          window.alert('you cancel the choose')
-
-        }
-
-      } else if ((title === "seat #4") && (selected)) {
-
-        if (window.confirm("Are you sure you want this seat?") === true) {
-          window.alert('you select the seat 4')
-          foo.className = `${className} disabledbutton`;
-          btn.disabled = true;
-        } else {
-          window.alert('you cancel the choose')
-
-        }
-      }
-    }
-
-
+    
     return (
-      <div className="col-sm-5">
+      <div >
         <div id={id}
           key={title}
           onClick={() => this.props.onClick()}
           className={className}>
           {title}
         </div>
-        <div className="col-sm-1"><button id={title} className="btn btn-success" onClick={() => confirm(this.props)}>click</button></div>
       </div>
     );
   }
