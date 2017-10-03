ea01fc6252dc19a6d5d88586e28fa0ccb31e1790
nelson torres <nelsondaniel88@gmail.com>
2017-10-03 16:42:14 -0400

    create the confirmation case here

--- src/reducers/reducer_seats.js
+++ src/reducers/reducer_seats.js
@@ -1,18 +1,25 @@
-import { SEAT_SELECTED } from '../actions/index'
+import { SEAT_SELECTED, SELECTED } from '../actions/index'
 // Esto no deberia existir, el initial deberia ser un array vacio
 // y cuando se haga el request al servidor se poblarian los seats
-const INITIAL_STATE = [ {id: 1, title: 'seat #1', selected: false},
-                        {id: 2, title: 'seat #2', selected: false},
-                        {id: 3, title: 'seat #3', selected: false},
-                        {id: 4, title: 'seat #4', selected: false}
-                      ]
+const INITIAL_STATE = [{ id: 1, title: 'seat #1', selected: false },
+                       { id: 2, title: 'seat #2', selected: false },
+                       { id: 3, title: 'seat #3', selected: false },
+                       { id: 4, title: 'seat #4', selected: false }
+]
+let className= `col-sm-8`
 
-export default function(state = INITIAL_STATE, action) {
-    switch (action.type){
-    case SEAT_SELECTED:
-        return state.map(seat => (seat.id === action.payload.selected)
-                            ? {...seat, selected: !seat.selected}
-                            : seat);
+export default function (state = INITIAL_STATE, action) {
+    switch (action.type) {
+        case SEAT_SELECTED:
+            return state.map(seat => (seat.id === action.payload.selected)
+                ? { ...seat, selected: !seat.selected }
+                : seat);
+                //este aun no esta completo y queria preguntarte unas cositas
+        case SELECTED:
+        return state.filter(seat =>((seat.id)&& (seat.selected))
+        ?alert('are you sure you want this seat??')        
+        :seat)
+           
         default:
             return state;
     }
