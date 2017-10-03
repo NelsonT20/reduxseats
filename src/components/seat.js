7323e9adaa4dd6cea952c72b915522a4abc7f3ba
nelson torres <nelsondaniel88@gmail.com>
2017-10-02 21:19:00 -0400

    add the function to block the seat element

--- src/components/seat.js
+++ src/components/seat.js
@@ -1,26 +1,78 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
-// import { connect } from 'react-redux';
 import '../style/style.css';
 
 class Seat extends Component {
   render() {
     const { id, title, selected } = this.props;
-    let className = `list-group-item seat-list`
-    if (selected) { 
-      className = `${className} selected` 
-    }else if(!selected){
-      className = `${className} unselected` 
+    let className = `list-group-item seat-list col-sm-6`
+
+    if (selected) {
+      className = `${className} selected`
+    } else if (!selected) {
+      className = `${className} unselected`
     }
-    
+
+    function confirm() {
+      let foo = document.getElementById(id);
+      let btn = document.getElementById(title);
+
+      if ((title === "seat #1") && (selected)) {
+
+        if (window.confirm("Are you sure you want this seat?") === true) {
+          window.alert('you select the seat 1')
+          foo.className = `${className} disabledbutton`;
+          btn.disabled = true;
+        } else {
+          window.alert('you cancel the choose')
+
+        }
+
+      } else if ((title === "seat #2") && (selected)) {
+
+        if (window.confirm("Are you sure you want this seat?") === true) {
+          window.alert('you select the seat 2')
+          foo.className = `${className} disabledbutton`;
+          btn.disabled = true;
+        } else {
+          window.alert('you cancel the choose')
+
+        }
+
+      } else if ((title === "seat #3") && (selected)) {
+
+        if (window.confirm("Are you sure you want this seat?") === true) {
+          window.alert('you select the seat 3')
+          foo.className = `${className} disabledbutton`;
+          btn.disabled = true;
+        } else {
+          window.alert('you cancel the choose')
+
+        }
+
+      } else if ((title === "seat #4") && (selected)) {
+
+        if (window.confirm("Are you sure you want this seat?") === true) {
+          window.alert('you select the seat 4')
+          foo.className = `${className} disabledbutton`;
+          btn.disabled = true;
+        } else {
+          window.alert('you cancel the choose')
+
+        }
+      }
+    }
+
+
     return (
-      <div>
+      <div className="col-sm-5">
         <div id={id}
           key={title}
           onClick={() => this.props.onClick()}
           className={className}>
           {title}
         </div>
+        <div className="col-sm-1"><button id={title} className="btn btn-success" onClick={() => confirm(this.props)}>click</button></div>
       </div>
     );
   }
