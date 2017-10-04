a5ea11f2ffb0f072458f5320ea0dfbd3b61b8811
nelson torres <nelsondaniel88@gmail.com>
2017-10-04 17:03:00 -0400

    add the confirm in proptypes and delete the variable infos

--- src/components/seat-list.js
+++ src/components/seat-list.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types'
 import Seat from './seat';
 import Confirm from './confirm';
 
-const SeatList = ({ seats, selectSeat, infos, Confirmation }) => (
+const SeatList = ({ seats, selectSeat, Confirmation }) => (
     <ul className="list-group col-sm-8">
         {seats.map((seat) => (
-            <div key={seat.id}>
+            <div key={seat.title}>
                 <div className="col-sm-7  seatlist ">
-                    <li key={seat.id}>
+                    <li >
                         <Seat key={seat.id}
                             {...seat}
                             onClick={() => selectSeat(seat)}
@@ -29,7 +29,8 @@ SeatList.propTypes = {
         PropTypes.shape({
             id: PropTypes.number.isRequired,
             selected: PropTypes.bool.isRequired,
-            title: PropTypes.string.isRequired
+            title: PropTypes.string.isRequired,
+            confirm: PropTypes.bool.isRequired
         }).isRequired
     ).isRequired,
     selectSeat: PropTypes.func.isRequired,
