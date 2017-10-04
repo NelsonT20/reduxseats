d81bb363498dc07c8bdc26ba221b8864adaf1d7e
nelson torres <nelsondaniel88@gmail.com>
2017-10-04 17:02:09 -0400

    nahjatze

--- src/components/confirm.js
+++ src/components/confirm.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux';
 
 class Confirm extends Component {
     render() {
+        //estara en comentarios mientras
+        // const { id, title, selected } = this.props;
+        // let { confirm } = this.props;
+
+        // if ((id) && (selected) && (confirm)) {
+        //     if (window.confirm('Are you sure you want this seat??') === true) {
+        //         window.alert('you choose this seat')
+        //     } else {
+        //         window.alert('You cancel the choose')
+        //     }
+        // }
+
 
         return (
             <div className="col-sm-2">
-                <button className="btn btn-success" onClick={()=> this.props.onClick()}>Confirm</button>
+                <button className="btn btn-success" onClick={() => this.props.onClick()}>Confirm</button>
             </div>
         )
     }
 
 }
+Confirm.propTypes = {
+    Confirmation: PropTypes.func.isRequired,
+  };
 
 export default Confirm;
