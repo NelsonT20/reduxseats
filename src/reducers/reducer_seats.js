2a215bb4d6e5e56e25724e7892608a2ec9fa41b6
nelson torres <nelsondaniel88@gmail.com>
2017-10-04 17:00:55 -0400

    Add the NEW_STATE and somo changes in the case confirm

--- src/reducers/reducer_seats.js
+++ src/reducers/reducer_seats.js
@@ -1,12 +1,13 @@
-import { SEAT_SELECTED, SELECTED } from '../actions/index'
+import { SEAT_SELECTED, CONFIRM } from '../actions/index'
 // Esto no deberia existir, el initial deberia ser un array vacio
 // y cuando se haga el request al servidor se poblarian los seats
-const INITIAL_STATE = [{ id: 1, title: 'seat #1', selected: false },
-                       { id: 2, title: 'seat #2', selected: false },
-                       { id: 3, title: 'seat #3', selected: false },
-                       { id: 4, title: 'seat #4', selected: false }
+const INITIAL_STATE = [{ id: 1, title: 'seat #1', selected: false, confirm: false },
+                       { id: 2, title: 'seat #2', selected: false, confirm: false },
+                       { id: 3, title: 'seat #3', selected: false, confirm: false },
+                       { id: 4, title: 'seat #4', selected: false, confirm: false }
 ]
-let className= `col-sm-8`
+let NEW_STATE =[]
+
 
 export default function (state = INITIAL_STATE, action) {
     switch (action.type) {
@@ -14,12 +15,14 @@ export default function (state = INITIAL_STATE, action) {
             return state.map(seat => (seat.id === action.payload.selected)
                 ? { ...seat, selected: !seat.selected }
                 : seat);
-                //este aun no esta completo y queria preguntarte unas cositas
-        case SELECTED:
-        return state.filter(seat =>((seat.id)&& (seat.selected))
-        ?alert('are you sure you want this seat??')        
-        :seat)
-           
+        case CONFIRM:
+            return( alert('are you sure?'),
+                state.map(seat => ((seat.id === action.payload.confirm) && seat.selected)
+                ? {...seat, confirm: !seat.confirm}
+                :seat).filter(seat=>((seat.id) && (seat.id) && (seat.confirm))
+                ?NEW_STATE.push(...seat)
+                :NEW_STATE)
+            )
         default:
             return state;
     }
